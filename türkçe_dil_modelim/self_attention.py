43b1ebe4b2a795c45915f8325b5e6f59d0284491
burakYilmazzZ1 <132089341+burakYilmazzZ1@users.noreply.github.com>
2026-02-02 10:43:00 +0000

    self attention yapısı başarıyla eklendi

---
+++ türkçe_dil_modelim/self_attention.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+class SelfAttention(nn.Module):
+    def __init__(self, embed_size, outputs_dim):
+        super().__init__()
+        self.embed_size = embed_size
+
+        self.q_weights= nn.Linear(embed_size, outputs_dim)
+        self.k_weights= nn.Linear(embed_size, outputs_dim)  
+        self.v_weights= nn.Linear(embed_size, outputs_dim)
+
+    def forward(self, x):
+        q= self.q_weights(x)
+        k= self.k_weights(x)
+        v= self.v_weights(x)
+
+        attention_scores = q @ k.T
+        attention_weights = torch.softmax(attention_scores / torch.sqrt(torch.tensor(k.shape[-1], dtype=torch.float32)), dim=1)
+        return attention_weights @ v
+
