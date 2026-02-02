97b22791e08efe90ee543228f79e133dbf0d610b
burakYilmazzZ1 <132089341+burakYilmazzZ1@users.noreply.github.com>
2026-02-02 10:26:55 +0000

    grafik çizme fonksiyonumun bazıbugları giderildi

--- türkçe_dil_modelim/plot_tokens.py
+++ türkçe_dil_modelim/plot_tokens.py
@@ -4,28 +4,32 @@ import plotly.offline
 
 def plot_tokens(sentences_data, title, dims=[0,1,2]):
 
-    data=[
+    data= [
         go.Scatter3d(
-            x=[s[dims[0]] for s in sentence_data['embeddings']],
-            y=[s[dims[1]] for s in sentence_data['embeddings']],
-            z=[s[dims[2]] for s in sentence_data['embeddings']],
+            x= sentence_data["words"][:, dims[0]],
+            y= sentence_data["words"][:, dims[1]],
+            z= sentence_data["words"][:, dims[2]],
             mode='markers+text',
-            text=sentence_data['labels'],
-            textposition="top center",
-            marker=dict(size=6, color=sentence_data["color"])
+            marker=dict(
+                size=6,
+                color=sentence_data["color"],
+            ),
+            text= sentence_data["labels"],
+            hoverinfo= "text",
+        
         )for sentence_data in sentences_data
     ]
     layout = go.Layout(
-        title=title,
         scene=dict(
-            xaxis_title='Dim {}'.format(dims[0]),
-            yaxis_title='Dim {}'.format(dims[1]),
-            zaxis_title='Dim {}'.format(dims[2]),
+            xaxis_title="hardness",
+            yaxis_title="brightness",
+            zaxis_title="redness",
         ),
-        title= title,
+        title=title,
         width=1000,
         height=1000,
-    )
+
+        )
 
     fig= go.Figure(data=data, layout=layout)
     plotly.offline.plot(fig)
