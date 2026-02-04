d4f4f58cfc3195d03008ab7af8c8b04dd2c5de36
burakYilmazzZ1 <132089341+burakYilmazzZ1@users.noreply.github.com>
2026-02-04 09:18:04 +0000

    güncelleme

--- türkçe_dil_modelim/causal_attention.py
+++ türkçe_dil_modelim/causal_attention.py
@@ -11,8 +11,10 @@ class CausalAttention(nn.Module):
         self.v_weights = nn.Linear(embed_size, output_size,bias=False)
         self.dropout = nn.Dropout(dropout_rate)
         self.register_buffer("mask", torch.tril(torch.ones(context_length, context_length)))
+        self.context_length= context_length
 
     def forward(self, x):
+        number_of_tokens= x.shape[0]
         context_length= x.shape[0]
         x= x[:context_length]
         Q = self.q_weights(x)  
@@ -20,7 +22,9 @@ class CausalAttention(nn.Module):
         V = self.v_weights(x)  
 
         attention_scores= K @ Q.transpose(-2, -1) 
-        masked_attention_scores= attention_scores.masked_fill(self.mask==0, -torch.inf)
+        masked_attention_scores= attention_scores.masked_fill(
+            self.mask[:number_of_tokens, :number_of_tokens] == 0, float('-inf')
+        )
         attention_weights= torch.softmax(masked_attention_scores/torch.sqrt(torch.tensor(self.embed_size, dtype=torch.float32)), dim=-1)
         attention_weights= self.dropout(attention_weights)
         return attention_weights @ V
