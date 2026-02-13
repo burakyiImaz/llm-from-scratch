e68766ffb3c95b00801a35cf1a8104471f063b55
burakYilmazzZ1 <132089341+burakYilmazzZ1@users.noreply.github.com>
2026-02-13 08:43:28 +0000

    text dataset batch halinde kaydedilmesi için güncelleme yapıldı

--- türkçe_dil_modelim/text_dataset.py
+++ türkçe_dil_modelim/text_dataset.py
@@ -1,5 +1,8 @@
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from türkçe_dil_modelim.tokenizer import Tokenizer
+from pathlib import Path
+
 
 class TextDataset(Dataset):
     def __init__(self, token_ids, context_length, stride):
@@ -32,6 +35,32 @@ def create_data_loader(token_ids: list, context_length:int, stride:int, batch_si
         dataset= TextDataset(token_ids, context_length, stride)
         data_loader= torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
         return data_loader
+def process_and_save_batches(file_path, tokenizer, context_length=32, stride=1, batch_size=64, save_dir="data_batches"):
+    save_dir= Path(save_dir)
+    save_dir.mkdir(exists_ok=True)
 
+    all_token_ids= []
 
+    with open(file_path,"r",encoding="utf") as f:
+        for line_idx, line in enumerate(f):
+              tokens= tokenizer.encode(line)
+              all_token_ids.extend(tokens.tolist())
+
+              if (line_idx + 1) % 1000 == 0:
+                print(f"{line_idx+1} satır işlendi, vocab boyutu: {tokenizer.get_vocab_size()}")
+    
+    tokenizer.save_vocab()
+
+    dataset = TextDataset(all_token_ids, context_length, stride)
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    # 3️⃣ Batch batch kaydet
+    batch_files = []
+    for batch_idx, (x, y) in enumerate(data_loader):
+        batch_file = save_dir / f"batch_{batch_idx}.pt"
+        torch.save({"input": x, "target": y}, batch_file)
+        batch_files.append(batch_file)
+    
+    print(f"Tüm batchler kaydedildi: {len(batch_files)} dosya")
+    return batch_files
 
