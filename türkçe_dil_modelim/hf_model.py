d5b406039a65f85da57321ff507e0d7992abc287
burakyiImaz <132089341+burakyiImaz@users.noreply.github.com>
2026-02-18 10:04:58 +0000

    casualLM düzenlemesi

--- türkçe_dil_modelim/hf_model.py
+++ türkçe_dil_modelim/hf_model.py
@@ -1,3 +1,5 @@
+import torch
+import torch.nn as nn
 from transformers import PreTrainedModel
 from .config import Config
 from .model import Model
@@ -7,12 +9,18 @@ class CasualLM(PreTrainedModel):
 
     def __init__(self,config):
         super().__init__(config)
+        self.model= Model(
+            vocab_size=config.vocab_size,
+            embedding_dim=config.embedding_dim,
+            num_heads=config.num_heads,
+            context_length=config.context_length,
+            num_layers=config.context_length,
+            device="cpu"
+        )
+        
+        self.lm_head= nn.Linear(config.embedding_dim, config.vocab_size,bias=False)
 
-        self.vocab_size= config.vocab_size
-        self.embedding_dim= config.embedding_dim
-        self.num_heads= config.num_heads
-        self.num_layers= config.num_layers
-        self.context_length= config.context_length
+        self.post_init() # HF weight için önemli
 
     def forward(self, input_ids):
         return self.model(input_ids)
