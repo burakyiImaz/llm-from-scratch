64eafb4f67ba55b60190ea8bb477df72cd172dc1
burakyiImaz <132089341+burakyiImaz@users.noreply.github.com>
2026-06-07 19:08:48 +0000

    rms_norm implemented

---
+++ türkçe_dil_modelim/normalization/rms_norm.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim, eps=1e-8):
+        super().__init__()
+
+        self.eps = eps
+
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x):
+
+        mean_square = x.pow(2).mean(dim=-1, keepdim=True)
+        rms = torch.sqrt(mean_square + self.eps)
+
+        x_norm = x / rms
+
+        return x_norm * self.weight
+
+
