b56a1d99a82b49bd2f7792a7d9af915a7b09f3f6
burakyiImaz <132089341+burakyiImaz@users.noreply.github.com>
2026-06-09 16:59:09 +0000

    I added bias for normalization but generally dont use bias in the rms_norm

--- türkçe_dil_modelim/normalization/rms_norm.py
+++ türkçe_dil_modelim/normalization/rms_norm.py
@@ -3,12 +3,14 @@ import torch.nn as nn
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, dim, eps=1e-8):
+    def __init__(self, embedding_dim, eps=1e-8,device="cpu"):
+
         super().__init__()
 
+        self.weights = nn.Parameter(torch.ones(embedding_dim,device=device))
+        self.bias= nn.Parameter(torch.zeros(embedding_dim,device=device))
         self.eps = eps
 
-        self.weight = nn.Parameter(torch.ones(dim))
 
     def forward(self, x):
 
@@ -17,6 +19,8 @@ class RMSNorm(nn.Module):
 
         x_norm = x / rms
 
-        return x_norm * self.weight
+        return x_norm * self.weights
+
+# eğer bias eklersem return x_norm* self.weights + bias yazabilirim ama standartta bias ile kullanılmıyor
 
 
